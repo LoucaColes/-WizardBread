e233c8ad8f431e518d5a0bedbbb30e583ba668e9
HarryJamesDavies <daviesharry1@gmail.com>
2017-10-26 22:43:09 +0100

    Simulation now checks every permutation of a list of numbers to find the 100 solutions with the highest score.

---
+++ WizardBread/Assets/Scripts/Improvements/Simulator/NumberPermutation.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NumberPermutation : MonoBehaviour
+{
+    public bool m_start = false;
+    public List<int> m_seed = new List<int>();
+    public static List<int> m_internalSeed = new List<int>();
+    public static List<List<int>> m_result = new List<List<int>>();
+
+    private static string m_filePath;
+    public static int m_currentLine = 0;
+    public static StreamReader m_reader = null;
+
+    void Update()
+    {
+        if (m_start)
+        {
+            m_start = false;
+            GenerateAllPermutations(m_seed);
+            SaveData();
+            m_result.Clear();
+        }
+    }
+
+    public static List<List<int>> GenerateAllPermutations(List<int> _seed)
+    {
+        m_internalSeed.Clear();
+        m_internalSeed.AddRange(_seed);
+        if (m_internalSeed.Count > 2)
+        {
+            for (int iter = 0; iter < m_internalSeed.Count; iter++)
+            {
+                GenerateRow(iter);
+            }
+        }
+        else if (m_internalSeed.Count == 2)
+        {
+            m_result.AddRange(GetPair(m_internalSeed[0], m_internalSeed[1]));
+        }
+        else if (m_internalSeed.Count == 1)
+        {
+            m_result.Add(m_internalSeed);
+        }
+        else
+        {
+            Debug.LogWarning("Seed is empty, no numbers to permutate");
+        }
+        return m_result;
+   }
+
+    private static void GenerateRow(int _row)
+    {
+        List<int> nextOut = new List<int>();
+        nextOut.AddRange(m_internalSeed);
+        nextOut.Remove(m_internalSeed[_row]);
+        nextOut.Insert(0, m_internalSeed[_row]);
+        m_result.AddRange(GetPermutation(nextOut));
+    }
+
+    private static List<List<int>> GetPermutation(List<int> _permIn)
+    {
+        List<List<int>> permOut = new List<List<int>>();
+        List<List<int>> permBack = new List<List<int>>();
+
+        if (_permIn.Count - 1 == 2)
+        {
+            permBack.AddRange(GetPair(_permIn[1], _permIn[2]));
+        }
+        else
+        {
+            List<int> nextSeed = new List<int>();
+            nextSeed.AddRange(_permIn);
+            nextSeed.RemoveAt(0);
+
+            for (int swapIter = 0; swapIter < nextSeed.Count; swapIter++)
+            {
+                List<int> nextPerm = new List<int>();
+                nextPerm.AddRange(nextSeed);
+                nextPerm.RemoveAt(swapIter);
+                nextPerm.Insert(0, nextSeed[swapIter]);
+
+                for (int permIter = 0; permIter < nextPerm.Count; permIter++)
+                {
+                    List<int> nextOut = new List<int>();
+                    nextOut.AddRange(nextPerm);
+                    nextOut.Remove(nextPerm[permIter]);
+                    nextOut.Insert(0, nextPerm[permIter]);
+                    nextPerm.Clear();
+                    permBack.AddRange(GetPermutation(nextOut));
+                    nextOut.Clear();
+                }
+            }
+        }
+
+        foreach (List<int> item in permBack)
+        {
+            List<int> result = new List<int>();
+            result.Add(_permIn[0]);
+            result.AddRange(item);
+            permOut.Add(result);
+        }
+        permBack.Clear();
+
+        return permOut;
+    }
+
+    private static List<List<int>> GetPair(int _a, int _b)
+    {
+        List<List<int>> permOut = new List<List<int>>();
+
+        List<int> a = new List<int>();
+        a.Add(_a);
+        a.Add(_b);
+        permOut.Add(a);
+        List<int> b = new List<int>();
+        b.Add(_b);
+        b.Add(_a);
+        permOut.Add(b);
+
+        return permOut;
+    }
+
+    private void SaveData()
+    {
+        m_filePath = "Assets/Data/Sim/Permutations/Size-" + m_internalSeed.Count + ".txt";
+
+        if (File.Exists(m_filePath))
+        {
+            File.Delete(m_filePath);
+        }
+
+        File.CreateText(m_filePath).Dispose();
+
+        StreamWriter writer = new StreamWriter(m_filePath, true);
+        writer.WriteLine("Permutations: " + m_result.Count);
+        foreach (List<int> item in m_result)
+        {
+            string Json = JsonUtility.ToJson(new PermData(item));
+            writer.WriteLine(Json);
+        }
+        writer.Close();
+        AssetDatabase.ImportAsset(m_filePath);
+    }
+
+    public static List<int> LoadData(int _length)
+    {
+        PermData data = null;
+        m_filePath = "Assets/Data/Sim/Permutations/Size-" + _length + ".txt";
+
+        if (File.Exists(m_filePath))
+        {
+            if (m_currentLine == 0)
+            {
+                m_reader = new StreamReader(m_filePath);
+                m_reader.ReadLine();
+                m_currentLine++;
+            }
+
+            string textIn = m_reader.ReadLine();
+            data = JsonUtility.FromJson<PermData>(textIn);
+            m_currentLine++;
+
+            if (m_reader.EndOfStream)
+            {
+                m_reader.Close();
+                m_currentLine = 0;
+            }
+
+        }
+        return data.Data;
+    }
+}
+
+[Serializable]
+class PermData
+{
+    public List<int> Data;
+
+    public PermData(List<int> _data)
+    {
+        Data = _data;
+    }
+}
--- WizardBread/Assets/Scripts/Improvements/Simulator/Simulator.cs
+++ WizardBread/Assets/Scripts/Improvements/Simulator/Simulator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Simulator : MonoBehaviour
 {
@@ -37,7 +39,7 @@ public class Simulator : MonoBehaviour
         {
             if(MaxLevel != 0)
             {
-                return (Level / MaxLevel) * 100.0f;
+                return ((float)Level / (float)MaxLevel) * 100.0f;
             }
             Debug.LogWarning(Name + " Result's MaxLevel is not set.");
             return 0.0f;
@@ -64,6 +66,7 @@ public class Simulator : MonoBehaviour
 
     public bool m_start = false;
     public int m_iteration = 0;
+    public int m_totalPermutations = 1;
     public List<ImproveTags> m_order = new List<ImproveTags>();
     public List<DataSet> m_results = new List<DataSet>();
 
@@ -72,23 +75,33 @@ public class Simulator : MonoBehaviour
     private List<ImprovementSim> m_activeSims = new List<ImprovementSim>();
     private List<ChangePackage> m_changeBuffer = new List<ChangePackage>();
 
-	void Update ()
+    public bool m_save = false;
+    public string m_name = "";
+    private string m_filePath;
+
+    void Update ()
     {
 		if(m_start)
         {
             m_start = false;
             BeginSimulation();
         }
+
+        if(m_save)
+        {
+            m_save = false;
+            SaveData();
+        }
 	}
 
     private void BeginSimulation()
     {
-        FirstOrder();
         m_results.Clear();
+        m_iteration = 0;
+        CalculateTotalPermutations();
 
-       // do
-        //{
-            m_iteration++;
+        while (!NextOrder())
+        {
             m_activeSims.Clear();
             m_results.Add(new DataSet());
 
@@ -109,15 +122,21 @@ public class Simulator : MonoBehaviour
             }
 
             m_results[m_results.Count - 1].CalculateScore();
-        //} while (!NextOrder());
+            CullResults();
+            m_iteration++;
+        }
+        m_activeSims.Clear();
+        Reorder(m_results[0].Order);
     }
 
-    private void FirstOrder()
+    private void CalculateTotalPermutations()
     {
-        m_order.Clear();
-        for(int iter = 0; iter <= (int)ImproveTags.Count - 1; iter++)
+        int max = m_order.Count;
+        m_totalPermutations = 1;
+
+        for(int iter = 0; iter < m_order.Count; iter++)
         {
-            m_order.Add((ImproveTags)iter);
+            m_totalPermutations *= (max - iter);
         }
     }
 
@@ -125,8 +144,33 @@ public class Simulator : MonoBehaviour
     {
         bool last = true;
 
+        List<int> next = NumberPermutation.LoadData(m_order.Count);
+        if (m_iteration < m_totalPermutations)
+        {
+            last = false;
+            Reorder(next);
+        }
 
         return last;
+    }
+
+    private void Reorder(List<int> _newOrder)
+    {
+        m_order.Clear();
+        foreach(int item in _newOrder)
+        {
+            m_order.Add((ImproveTags)item);
+        }
+    }
+
+    private void CullResults()
+    {
+        if(m_results.Count > 100)
+        {
+            m_results.Sort((a, b) => a.Score.CompareTo(b.Score));
+            m_results.RemoveRange(100, m_results.Count - 100);
+            m_results.Reverse();
+        }
     }
 
     private void AddImprovement(ImproveTags _tag)
@@ -158,5 +202,79 @@ public class Simulator : MonoBehaviour
         {
             m_changeBuffer.AddRange(tempBuffer);
         }
+    }
+
+    private void SaveData()
+    {
+        if (m_name != "")
+        {
+            m_filePath = "Assets/Data/Sim/Results/" + m_name + ".txt";
+
+            int pathIter = 0;
+            string name = m_name;
+            while (File.Exists(m_filePath))
+            {
+                pathIter++;
+                name = m_name + pathIter;
+                m_filePath = "Assets/Data/Sim/Results/" + name + ".txt";
+            }
+            m_name = "";
+
+            File.CreateText(m_filePath).Dispose();
+
+            StreamWriter writer = new StreamWriter(m_filePath, true);
+
+            writer.WriteLine("Name: " + name);
+            writer.WriteLine("");
+            writer.WriteLine("================================");
+            writer.WriteLine("");
+
+            int dataIter = 1;
+            foreach (DataSet set in m_results)
+            {
+                writer.WriteLine("Set: " + dataIter);
+                writer.WriteLine("");
+
+                writer.Write("Order: " + set.Order[0]);
+                for (int orderIter = 1; orderIter < set.Order.Count; orderIter++)
+                {
+                    writer.Write(", " + set.Order[orderIter]);
+                }
+                writer.WriteLine("");
+                writer.WriteLine("");
+
+                writer.WriteLine("Score: " + set.Score);
+                writer.WriteLine("");
+
+                foreach (ImproveResult result in set.Results)
+                {
+                    writer.WriteLine("Improvement: " + result.Name);
+                    writer.WriteLine("Level: " + result.Level);
+                    writer.WriteLine("MaxLevel: " + result.MaxLevel);
+                    writer.WriteLine("");
+                }
+                
+                writer.WriteLine("================================");
+                writer.WriteLine("");
+                dataIter++;
+            }
+            writer.Close();
+            AssetDatabase.ImportAsset(m_filePath);
+        }
+        else
+        {
+            Debug.Log("Results file needs name.");
+        }
+    }
+}
+
+[Serializable]
+class OrderData
+{
+    public List<int> Data = new List<int>();
+
+    public OrderData(List<int> _data)
+    {
+        Data.AddRange(_data);
     }
 }
