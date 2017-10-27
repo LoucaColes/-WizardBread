6fa629d22305ab6d705bdea809bb1643f739bfe8
Yoni Higgsmith <yoni.higgsmith@gmail.com>
2017-10-27 20:46:32 +0100

    Instantiate Level 1 Objects with smoke effect based on UI

---
+++ WizardBread/Assets/Scripts/Chosen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chosen : MonoBehaviour {
+
+    public Sprite whiteblock;
+    public GameObject townObject;
+    
+    public int ID_Number;
+
+    private GameObject m_improvementsParent;
+    
+
+
+    private void Start()
+    {
+        m_improvementsParent = GameObject.Find("Improvements");
+        if (!m_improvementsParent)
+        {
+            m_improvementsParent = new GameObject("Improvements");
+        } 
+        
+    }
+       
+    public void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = whiteblock;
+        Debug.Log(name + " pressed");
+        
+
+        GameObject newTownObject = Instantiate(townObject) as GameObject;
+       // _anim = newTownObject.FindComponentOfType<Animation>();
+        newTownObject.transform.parent = m_improvementsParent.transform;
+        newTownObject.transform.position = townObject.transform.position;
+
+    }
+}
