a22c544c821ff85e2f0d8d82dfbb019025bac1b8
Yoni Higgsmith <yoni.higgsmith@gmail.com>
2017-11-02 18:29:54 +0000

    added tooltipUI script (can be added to any object and string assigned) and a new handy sign to keep tooltips on.

---
+++ WizardBread/Assets/Scripts/ToolTipUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTipUI : MonoBehaviour
+{
+
+    public string m_string;
+    private Text m_toolTipText;
+    public float m_fadeTime = 5.0f;
+
+    private void Start()
+    {
+        m_toolTipText = GameObject.Find("ToolTipText").GetComponent<Text>();
+        m_toolTipText.color = Color.clear;
+    }
+
+    private void OnMouseOver()
+    {
+        m_toolTipText.color = Color.Lerp(m_toolTipText.color, Color.black, m_fadeTime * Time.deltaTime);
+        m_toolTipText.text = m_string;
+    }
+}
+
