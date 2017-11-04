66d55f54b99b53f80193255c110426ee38de3e91
LoucaColes <coleslouca@yahoo.co.uk>
2017-11-04 19:42:11 +0000

    Updated Game Over Canvas

--- WizardBread/Assets/Scripts/GameOverCanvas.cs
+++ WizardBread/Assets/Scripts/GameOverCanvas.cs
@@ -8,17 +8,21 @@ public class GameOverCanvas : MonoBehaviour
 {
     public Canvas m_gameOverCanvas;
     public Text m_esteemText;
+    public Text m_messageText;
 
     public float m_time;
     private float m_timer;
 
     private bool m_played;
 
+    public int m_targetScore;
+
     // Use this for initialization
     private void Start()
     {
         m_played = false;
         m_esteemText.text = "";
+        m_messageText.text = "";
         m_timer = 0;
     }
 
@@ -29,13 +33,15 @@ public class GameOverCanvas : MonoBehaviour
         {
             if (!m_played)
             {
-                if (Town.m_instance.m_townSelfEsteem >= 750)
+                if (Town.m_instance.m_townSelfEsteem >= m_targetScore)
                 {
                     AudioManager.m_instance.PlaySFX("Win");
+                    m_messageText.text = "Well Done! You Reached The Target Score";
                 }
                 else
                 {
                     AudioManager.m_instance.PlaySFX("GameOver");
+                    m_messageText.text = "Unlucky! Try Again!";
                 }
                 m_played = true;
             }
