d33e3627901b0901dd6bbc2badd8124046c86b74
HarryJamesDavies <daviesharry1@gmail.com>
2017-11-14 18:29:28 +0000

    Highscores now default fill

--- WizardBread/Assets/Scripts/GameOverCanvas.cs
+++ WizardBread/Assets/Scripts/GameOverCanvas.cs
@@ -9,6 +9,8 @@ public class GameOverCanvas : MonoBehaviour
     public Canvas m_gameOverCanvas;
     public Text m_esteemText;
     public Text m_messageText;
+    public List<Highscore> m_highscores = new List<Highscore>();
+    public List<Sprite> m_sprites = new List<Sprite>();
 
     public float m_time;
     private float m_timer;
@@ -36,13 +38,25 @@ public class GameOverCanvas : MonoBehaviour
                 if (Town.m_instance.m_townSelfEsteem >= m_targetScore)
                 {
                     AudioManager.AudioManager.m_instance.PlaySFX("Win", Vector3.zero);
-                    m_messageText.text = "Well Done! You Reached The Target Score";
+                    m_messageText.text = "Well Done! Goal Achieved";
                 }
                 else
                 {
                     AudioManager.AudioManager.m_instance.PlaySFX("GameOver", Vector3.zero);
                     m_messageText.text = "Unlucky! Try Again!";
                 }
+
+                for(int iter = 0; iter <= m_highscores.Count - 1; iter++)
+                {
+                    m_highscores[iter].Position.text = Highscore.GetPosition(iter);
+                    m_highscores[iter].Score.text = "" + 100 + iter;
+
+                    for (int iconIter = 0; iconIter <= m_sprites.Count - 1; iconIter++)
+                    {
+                        m_highscores[iter].Icons[iconIter].sprite = m_sprites[iconIter];
+                    }
+                }
+
                 m_played = true;
             }
             m_gameOverCanvas.enabled = true;
@@ -55,6 +69,17 @@ public class GameOverCanvas : MonoBehaviour
         }
         else
         {
+            for (int iter = 0; iter <= m_highscores.Count - 1; iter++)
+            {
+                m_highscores[iter].Position.text = "---";
+                m_highscores[iter].Score.text = "---";
+
+                for (int iconIter = 0; iconIter <= m_sprites.Count - 1; iconIter++)
+                {
+                    m_highscores[iter].Icons[iconIter].sprite = null;
+                }
+            }
+
             m_gameOverCanvas.enabled = false;
         }
     }
---
+++ WizardBread/Assets/Scripts/Highscore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+    public Text Position;
+    public Text Score;
+    public List<Image> Icons = new List<Image>();
+
+    public static string GetPosition(int _pos)
+    {
+        string position = _pos.ToString();
+        switch (_pos)
+        {
+            case 1:
+                {
+                    position += "st";
+                    break;
+                }
+            case 2:
+                {
+                    position += "nd";
+                    break;
+                }
+            case 3:
+                {
+                    position += "rd";
+                    break;
+                }
+            default:
+                {
+                    position += "th";
+                    break;
+                }
+        }
+        return position;
+    }
+}
